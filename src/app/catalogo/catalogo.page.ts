0b35d0737deeece0d52c0b12d4823d34c7ef9eba
vBookerM <mat.carvajal@duocuc.cl>
2023-01-06 12:51:54 -0300

    FINAL

--- src/app/catalogo/catalogo.page.ts
+++ src/app/catalogo/catalogo.page.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, NavigationExtras } from '@angular/router';
+import { Component} from '@angular/core';
+import { ActivatedRoute,NavigationExtras } from '@angular/router';
 @Component({
   selector: 'app-catalogo',
   templateUrl: './catalogo.page.html',
   styleUrls: ['./catalogo.page.scss'],
 })
-export class CatalogoPage implements OnInit {
+export class CatalogoPage {
 
-  constructor() { }
+  constructor(
 
-  ngOnInit() {
+  ) { }
+
+  ngOnInit(
+  ) {
   }
+
 
 }
